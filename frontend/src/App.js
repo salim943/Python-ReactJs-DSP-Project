03734d2aadeb7333a979ae19c7d1e1e94be98c54
salim <67800072+salim943@users.noreply.github.com>
2025-05-01 17:20:40 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './Sidebar.js';
@@ -7,16 +6,15 @@ import AM_demodulation from './AM_demodulation';
 import FM from './FM';
 import PM from './PM';
 import Dsbsc from './Dsbsc';
-import YourComponent from './YourComponent';
+
+
 
 function App() {
   return (
     <div>
-
       <Router>
         <div>
           <Sidebar />
-
           <Routes>
             <Route path="/AM" element={<Circuit />} />
             <Route path="/AM_demodulation" element={<AM_demodulation />} />
@@ -26,26 +24,27 @@ function App() {
           </Routes>
         </div>
       </Router>
+
       <h2>Welcome to Signal Processing Simulator App</h2>
-	   <div class="box">
-    <h3>Useful Links</h3>
-    <ul>
-      <li><a href="/AM">Amplitude Modulation (AM)</a></li>
-      <li><a href="/FM">Frequency Modulation (FM)</a></li>
-	  <li><a href="/PM">Phase Modulation (PM)</a></li>
-    </ul>
-  </div>
- 
-      {/* Uncomment to display the components */}
+
+      <div className="box">
+        <h3>Useful Links</h3>
+        <ul>
+          <li><a href="/AM">Amplitude Modulation (AM)</a></li>
+          <li><a href="/FM">Frequency Modulation (FM)</a></li>
+          <li><a href="/PM">Phase Modulation (PM)</a></li>
+        </ul>
+      </div>
+
+      {/* Optional: Uncomment for development testing */}
       {/* 
       <Circuit />
       <AM_demodulation />
       <FM />
-      */}
-      
-      {/* <div className="App1">
+      <div className="App1">
         <YourComponent />
-      </div> */}
+      </div> 
+      */}
     </div>
   );
 }
